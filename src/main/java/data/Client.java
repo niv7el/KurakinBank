635b236c9e289db0ad295485d6288ab20e8974d1
niv7el <nkurakin.cl@gmail.com>
2024-03-28 16:18:02 +0300

    Client class realization

--- src/main/java/data/Client.java
+++ src/main/java/data/Client.java
@@ -1,5 +1,32 @@
 package data;
+import bank.Card;
 
 public class Client {
-    //
+    private String name, surname;
+    private boolean isClient;
+    private Card card;
+
+    public void setClient(boolean client) {
+        isClient = client;
+    }
+
+    public void setCard(Card card) {
+        this.card = card;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public boolean isClient() {
+        return isClient;
+    }
+
+    public Card getCard() {
+        return card;
+    }
 }
